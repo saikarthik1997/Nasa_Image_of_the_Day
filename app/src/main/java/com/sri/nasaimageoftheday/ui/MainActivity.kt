14d1ced6aa7559ac40d0979713a8ba118b9aba57
Sai Karthik <saikarthiksistla@gmail.com>
2023-09-09 02:51:56 +0530

    api called and result printed

--- app/src/main/java/com/sri/nasaimageoftheday/ui/MainActivity.kt
+++ app/src/main/java/com/sri/nasaimageoftheday/ui/MainActivity.kt
@@ -2,11 +2,20 @@ package com.sri.nasaimageoftheday.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.lifecycle.ViewModelProvider
 import com.sri.nasaimageoftheday.R
+import com.sri.nasaimageoftheday.view_models.MainViewModel
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
+    private lateinit var mainViewModel: MainViewModel
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        mainViewModel = ViewModelProvider(this)[MainViewModel::class.java]
+        Log.i("MyTag",mainViewModel.toString())
         setContentView(R.layout.activity_main)
+        mainViewModel.fetchImages()
     }
 }
--- app/src/main/java/com/sri/nasaimageoftheday/view_models/MainViewModel.kt
+++ app/src/main/java/com/sri/nasaimageoftheday/view_models/MainViewModel.kt
@@ -1,4 +1,29 @@
 package com.sri.nasaimageoftheday.view_models
 
-class MainViewModel {
+import android.app.Application
+import android.util.Log
+import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.viewModelScope
+import com.sri.nasaimageoftheday.data.Repository
+import com.sri.nasaimageoftheday.utils.Constants
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+
+@HiltViewModel
+class MainViewModel @Inject constructor(
+    private val repository: Repository,
+    application: Application
+): AndroidViewModel(application){
+
+    fun fetchImages(){
+       val queries: HashMap<String, String> = HashMap()
+       queries["api_key"]= Constants.API_KEY
+       queries["count"]="20"
+        viewModelScope.launch {
+            val response =   repository.remote.getImages(queries)
+            Log.i("MyTag","response is ${response.body()}")
+
+   }}
 }
